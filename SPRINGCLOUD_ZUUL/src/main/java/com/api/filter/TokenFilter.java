757392ea855ebf4b2a47bfe00c4cdab8b15f12ac
qipingzhiji <1213462884@qq.com>
2019-05-09 20:16:53 +0800

    增加zuul过滤规则。

---
+++ SPRINGCLOUD_ZUUL/src/main/java/com/api/filter/TokenFilter.java
@@ -0,0 +1,42 @@
+package com.api.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TokenFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    //控制过滤器生效不生效，可以在里面写一串逻辑来控制
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        String token = request.getParameter("token");
+        if (token == null){
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            context.setResponseBody("unAuthrized");
+
+
+            return null;
+        }
+        return null;
+    }
+
+}
