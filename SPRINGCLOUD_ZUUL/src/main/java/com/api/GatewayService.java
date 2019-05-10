8837a82c2be4bc94a0dc66959fed7b10c396887c
qipingzhiji <1213462884@qq.com>
2019-05-10 11:40:06 +0800

    增加zuul过滤规则。

--- SPRINGCLOUD_ZUUL/src/main/java/com/api/GatewayService.java
+++ SPRINGCLOUD_ZUUL/src/main/java/com/api/GatewayService.java
@@ -1,17 +1,31 @@
 package com.api;
 
+import com.api.filter.TokenFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude=
         {DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @EnableZuulProxy
-
 public class GatewayService {
     public static void main(String[] args) {
         new SpringApplicationBuilder(GatewayService.class).web(true).run(args);
     }
+
+    //将过滤器交给Spring管理
+    @Bean
+    public TokenFilter tokenFilter() {
+        return new TokenFilter();
+    }
+
+    //zuul自定义添加路由规则
+    /*@Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        return new PatternServiceRouteMapper("","");
+    }*/
 }
--- SPRINGCLOUD_ZUUL/src/main/java/com/api/filter/TokenFilter.java
+++ SPRINGCLOUD_ZUUL/src/main/java/com/api/filter/TokenFilter.java
@@ -7,11 +7,29 @@ import javax.servlet.http.HttpServletRequest;
 
 public class TokenFilter extends ZuulFilter {
 
+    //四种类型：pre,routing,error,post
+    //pre：主要用在路由映射的阶段是寻找路由映射表的
+    //routing:具体的路由转发过滤器是在routing路由器，具体的请求转发的时候会调用
+    //error:一旦前面的过滤器出错了，会调用error过滤器。
+    //post:当routing，error运行完后才会调用该过滤器，是在最后阶段的
+
+    /**
+     * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+     *  1.pre：可以在请求被路由之前调用，用在路由映射的阶段是寻找路由映射表的
+
+     　　2.route：在路由请求时候被调用，具体的路由转发过滤器是在routing路由器具体的请求转发的时候会调用
+
+     　　3.error：处理请求时发生错误时被调用
+
+     　　4.post：当routing，error运行完后才会调用该过滤器，是在最后阶段的
+     * @return
+     */
     @Override
     public String filterType() {
-        return null;
+        return "pre";
     }
 
+    //自定义过滤器执行的顺序，数值越大越靠后执行，越小就越先执行
     @Override
     public int filterOrder() {
         return 0;
@@ -23,6 +41,7 @@ public class TokenFilter extends ZuulFilter {
         return true;
     }
 
+    //执行过滤逻辑
     @Override
     public Object run() {
         RequestContext context = RequestContext.getCurrentContext();
